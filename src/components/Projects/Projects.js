681a55288fd0323f49fad9bbae7095fe5dbc5d7d
Siddhesh Jadhav <siddheshjadhav60@gmail.com>
2024-12-24 15:53:21 +0530

    Initial Files

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -35,23 +35,23 @@ function Projects() {
             <ProjectCard
               imgPath={Tweet}
               title="Twitlaytics"
-              description=""            
+              description="Build a website to analyze the tweets of user or company to know the sentiment ofthe tweet whether it is Positive, Negative or Neutral. Using Twitter Developer account and Python Language"            
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={url}
-              title="URL Shortner"
-              description=""
+              title="Yourls"
+              description="Which stands for Your Own URL Shortener, is a GUI-based project. This URL shortener isdesigned to transform lengthy links into shorter ones for easier comprehension. The project isimplemented in Python."
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={PG}
-              title="Password Generator"
-              description=""
+              title="Random Password Generator"
+              description="A GUI-Based Project that Provides Users with Random Passwords.Java language is used to build this Project."
             />
           </Col>
 
@@ -59,7 +59,7 @@ function Projects() {
             <ProjectCard
               imgPath={currencyconvertor}
               title="Currency Convertor"  
-              description=""
+              description="This project simplifies currency conversion by allowing users to convert local amounts to international currencies with just a few clicks. It fetches the latest exchange rates using a public API and leverages the requests library for API interaction. The GUI is built using the tkinter library with components like buttons, labels, and textboxes."
             />
           </Col>
 
@@ -67,7 +67,7 @@ function Projects() {
             <ProjectCard
               imgPath={todolist}
               title="To Do List"
-              description=""
+              description="This Todo List app, built with React, allows users to easily manage tasks by adding, editing, and deleting items. It features a clean, user-friendly interface and dynamically updates the task list, offering a seamless experience for organizing daily activities and staying on top of priorities."
            />
           </Col>
 
@@ -75,7 +75,7 @@ function Projects() {
             <ProjectCard
               imgPath={Tictactoe}
               title="Tic-Tac_Toe"
-              description=""
+              description="This Tic-Tac-Toe game, built with React, offers a simple and interactive way to play the classic game. It features a responsive grid, player turn tracking, and a win condition check, providing an engaging user experience. The app is fully functional with state management handled using React hooks."
             />
           </Col>
         </Row>
